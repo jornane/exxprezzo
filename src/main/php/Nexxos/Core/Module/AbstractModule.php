31a24b8c18b33a95823bff96f3587592d79885ef
Wilfried van Asten <wilfriedvanasten@gmail.com>
2012-05-08 19:46:36 +0200

    Unfinished implementation of getFunctionPath

--- src/main/php/Nexxos/Core/Module/AbstractModule.php
+++ src/main/php/Nexxos/Core/Module/AbstractModule.php
@@ -39,8 +39,44 @@ namespace Nexxos\Core\Module {
 			return $this->urlManager;
 		}
 		
-		public function getFunctionPath() {
-			
+		public static function getFunctionPath($function, $args) {
+			if(!isset(static::$paths))
+				user_error("This module has no usable paths");
+			if(!isset($args[$function]))
+				user_error("This function has no paths in this module");
+			$paths = static::$paths[$function];
+			$result = NULL;
+			for($paths as $path) {
+				$vars = extractVars($path);i
+				if($vars === array_keys($args)) {
+					
+				}
+			}
+		}
+		
+
+		/**
+		 * Extracts the variables from a given path
+		 * 
+		 * The path is a path string in which variable
+		 * segments are given as {$name}. This will
+		 * return a set of all names encountered in
+		 * the path
+		 * @param string $path The path to extract
+		 * the variables from
+		 * @return array A set of the variable names contained
+		 * in path
+		 */
+		private static function extractVars($path) {
+			const $regex = '/^.*({\\$(?<name>.*)}.*)*$/';
+			$mathes = array ()
+			preg_match($regex,$path,$mathes);
+			$names = $mathes['name'];
+			$result = array();
+			for($names as $name){
+				$result[$name] = NULL;
+			}
+			return array_keys($result);
 		}
 	}
 }
