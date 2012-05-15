4b62ae1491ac6a0c5733019d3f73bf977ce09fb1
Yørn <git@yorn.priv.no>
2012-05-16 01:21:34 +0200

    Added phpdoc
    Documented Output interface

--- src/main/php/exxprezzo/core/Output.php
+++ src/main/php/exxprezzo/core/Output.php
@@ -1,8 +1,40 @@
 <?php namespace exxprezzo\core;
 
+use exxprezzo\core\module\AbstractModule;
+
 interface Output extends Runnable {
+	
+	/**
+	 * Send this Output to the browser rightaway
+	 * 
+	 * (non-PHPdoc)
+	 * @see exxprezzo\core.Runnable::run()
+	 */
+	function run();
+	
+	/**
+	 * @return AbstractModule
+	 */
+	function getSource();
+	
+	/**
+	 * @return string
+	 */
 	function renderContents();
+	
+	/**
+	 * @return int
+	 */
 	function getLength();
+	
+	/**
+	 * @return \DateTime
+	 */
 	function getLastModified();
+	
+	/**
+	 * @return \DateTime
+	 */
 	function getExpiryDate();
+	
 }
