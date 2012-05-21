5c6dbd33b4ffa01f53b7da0aa66fb68890e79c43
Yørn <git@yorn.priv.no>
2012-05-21 19:48:55 +0200

    Made Template better readable

--- src/main/php/exxprezzo/core/Template.php
+++ src/main/php/exxprezzo/core/Template.php
@@ -16,8 +16,13 @@ class Template {
 	
 	const REGEX_BLOCK = '_\\<\\!\\-\\- ([A-Z0-9\\_\\-]+) ([a-z0-9\\.\\_\\-]+) \\-\\-\\>(.*?)\\<\\!\\-\\- /\\1 \\2 \\-\\-\\>_ms';
 	const REGEX_ANNOTATION = '_\\<\\!\\-\\- ([a-z0-9\\.\\_\\-]+) ([a-z0-9\\.\\_\\-]+) \\-\\-\\>_i';
-	const REGEX_VAR = '_\\{([a-z0-9\\.\\_\\-]*?)\\}_i'; // {VAR} and {iteration.VAR}
+	const KEYWORD = 1;
+	const BLOCKNAME = 2;
 	
+	const REGEX_VAR = '_\\{([a-z0-9\\.\\_\\-]*?)\\}_i'; // {VAR} and {iteration.VAR}
+	const VARNAME = 1;
+	
+	const REGEX_COMMENT = '_\\<\\!\\-\\- (.*) \\-\\-\\>_i';
 	
 	public function __construct($templateCode) {
 		$this->templateCode = $templateCode;
@@ -28,8 +33,8 @@ class Template {
 			return $this->blocks;
 		preg_match_all(self::REGEX_BLOCK, $this->templateCode, $matches);
 		if (is_null($this->blockKeywords))
-			$this->blockKeywords = array_unique($matches[1]);
-		return $this->blocks = array_unique($matches[2]);
+			$this->blockKeywords = array_unique($matches[self::KEYWORD]);
+		return $this->blocks = array_unique($matches[self::BLOCKNAME]);
 	}
 	
 	public function getAnnotations() {
@@ -38,7 +43,7 @@ class Template {
 		if (is_null($this->blockKeywords))
 			$this->getBlocks();
 		preg_match_all(self::REGEX_ANNOTATION, $this->templateCode, $matches);
-		$result = array_combine($matches[1], $matches[2]);
+		$result = array_combine($matches[self::KEYWORD], $matches[self::BLOCKNAME]);
 		foreach($this->blockKeywords as $key)
 			unset($result[$key]);
 		return $this->annotations = $result;
@@ -48,7 +53,7 @@ class Template {
 		if (!is_null($this->variables))
 			return $this->variables;
 		preg_match_all(self::REGEX_VAR, $this->templateCode, $matches);
-		return $this->variables = array_unique($matches[1]);
+		return $this->variables = array_unique($matches[self::VARNAME]);
 	}
 	
 	public function render($content) {
