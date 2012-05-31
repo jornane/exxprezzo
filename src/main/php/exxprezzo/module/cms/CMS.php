0bafe7c08f02e317bd919b7a3e0e8d259d76e43b
Yørn <git@yorn.priv.no>
2012-05-31 14:27:31 +0200

    Improved CMS

--- src/main/php/exxprezzo/module/cms/CMS.php
+++ src/main/php/exxprezzo/module/cms/CMS.php
@@ -1,5 +1,7 @@
 <?php namespace exxprezzo\module\cms;
 
+use exxprezzo\core\Core;
+
 use exxprezzo\core\Content;
 
 use exxprezzo\core\output\ContentOutput;
@@ -8,13 +10,26 @@ use exxprezzo\core\module\AbstractModule;
 
 class CMS extends AbstractModule {
 	
+	protected $db;
+	
 	protected static $functions = array(
 			'(.*)' => 'view',
 		);
 	
 	public function view() {
+		$db = $this->getModuleParam();
 		$content = new Content();
-		$content->putVariable('FOO', 'BAR');
+		$params = $this->getParameters();
+		$db->execute('SELECT `title`, `content` FROM `pages` WHERE `path` = $path LIMIT 1', array(
+				'path' => ltrim($params['path'], '/'),
+			));
+		if($page = $db->fetchrow()) {
+			$content->putVariables(array(
+					'CONTENT' => $page['content'],
+				));
+		} else {
+			user_error('Page not found');
+		}
 		return new ContentOutput($this, $content);
 	}
 	
