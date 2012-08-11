047320d0b1a1f762d8d59c2ef3b9ffa483a0abc4
Yørn <git@yorn.priv.no>
2012-08-11 17:30:16 +0200

    Added SessionManager module

---
+++ src/main/php/exxprezzo/module/sessionmanager/Session.php
@@ -0,0 +1,125 @@
+<?php namespace exxprezzo\module\session;
+
+use \exxprezzo\core\db\SQL;
+
+use \exxprezzo\core\Core;
+
+class Session {
+	
+	/** @var \exxprezzo\module\sessionmanager\SessionManager */
+	protected $sessionManager;
+	/** @var string */
+	protected $sid;
+	/** @var int */
+	protected $moduleId;
+	/** @var int */
+	protected $defaultLifetime = 3600;
+	/** @var SQL */
+	protected $db;
+	
+	protected function __construct($sessionManager, $moduleId, $sid=NULL) {
+		$this->sessionManager = $sessionManager;
+		$this->moduleId = $moduleId;
+		$this->sid = $sid;
+	}
+	
+	public function __set($key, $value) {
+		if (is_null($this->sid))
+			$this->sid = $this->randomString(64);
+		$this->db->replace('var', array(
+				'session' => $this->sid,
+				'moduleInstance' => $this->moduleId,
+				'key' => $key,
+				'value' => serialize($value),
+				'touched' => time(),
+			));
+		$this->updateCookie();
+	}
+	public function __get($key) {
+		if (is_null($this->sid))
+			return NULL;
+		$result = $this->db->query('SELECT `value` FROM `var`
+				WHERE `key` = $key
+				AND `session` = $session
+				AND `moduleInstance` = $moduleInstance
+				AND `touched`+`lifetime` < $now', array(
+						'key' => $key,
+						'session' => $this->sid,
+						'moduleInstance' => $this->moduleId,
+						'now' => time(),
+			));
+		return isset($result[0]['value']) ? $result[0]['value'] : NULL;
+	}
+	public function __isset($key) {
+		if (is_null($this->sid))
+			return false;
+		$result = $this->db->execute('SELECT `value` FROM `var`
+				WHERE `key` = $key
+				AND `session` = $session
+				AND `moduleInstance` = $moduleInstance
+				AND `touched`+`lifetime` < $now', array(
+						'key' => $key,
+						'session' => $this->sid,
+						'moduleInstance' => $this->moduleId,
+						'now' => time(),
+			));
+		return (bool)$this->db->numrows();
+	}
+	public function __unset($key) {
+		if (is_null($this->sid))
+			return;
+		$this->db->delete('var', array(
+				'key' => $key,
+				'session' => $this->sid,
+				'moduleInstance' => $this->moduleId,
+			));
+		$this->updateCookie();
+	}
+	
+	public function setLifetime($key, $lifetime) {
+		if (is_null($this->sid))
+			return;
+		$this->db->replace('var', array(
+				'session' => $this->sid,
+				'moduleInstance' => $this->moduleId,
+				'key' => $key,
+				'lifetime' => (int)$lifetime,
+				'touched' => time(),
+		));
+		$this->updateCookie();
+	}
+	
+	protected function randomString($length) {
+		$str = '';
+		for($i=0;$i<((int)($length*0.75));$i++)
+			$str .= chr(mt_rand(0,255));
+		return base64_encode($str);
+	}
+	
+	protected function updateCookie() {
+		if (is_null($this->sid))
+			return;
+		$result = $this->db->query('SELECT MAX(`touched`+`lifetime`) `max` FROM `var`
+				WHERE `key` = $key
+				AND `session` = $session
+				AND `moduleInstance` = $moduleInstance', array(
+						'key' => $key,
+						'session' => $this->sid,
+						'moduleInstance' => $this->moduleId,
+			));
+		$expire = isset($result[0]['max']) ? $result[0]['max'] : 0;
+		setcookie(
+				$this->sessionManager->session_cookie_name,
+				$this->sid,
+				$expire,
+				$this->sessionManager->session_cookie_path,
+				'',
+				false,
+				true
+			);
+		if ($uri->isInCookie($this->sessionManager->session_cookie_name))
+			Core::getUrlManager()->forcePostVariable($this->sessionManager->session_cookie_name, $this->sid);
+		else
+			Core::getUrlManager()->forceVariable($this->sessionManager->session_cookie_name, $this->sid);
+	}
+}
---
+++ src/main/php/exxprezzo/module/sessionmanager/SessionManager.php
@@ -0,0 +1,94 @@
+<?php namespace exxprezzo\module\sessionmanager;
+
+use exxprezzo\core\url\HostGroup;
+
+use exxprezzo\module\session\Session;
+
+use exxprezzo\core\Core;
+
+use exxprezzo\core\module\AbstractModule;
+
+class SessionManager extends AbstractModule {
+	
+	protected $sid;//session id
+	protected $session_cookie_name;
+	
+	protected $allowInsecurePost = true;
+	
+	public function init() {
+		parent::init();
+		
+		$this->cookie_name = 'SESSION';
+		
+		$uri = Core::getUrlManager();
+		
+		if ($uri->isGet()) {
+			/**
+			 * When the SID is available both as cookie and get parameter,
+			 * kill the get parameter and reload
+			 *
+			 * The get and cookie may not be the same, but cookie has preference over get,
+			 * and the GET parameter is the one being eliminated.
+			 */
+			if($uri->isInCookie($this->session_cookie_name)
+					&& $uri->isInGet($this->session_cookie_name)) {
+				$get = $uri->getRawGet();
+				unset($get[$this->session_cookie_name]);
+				$uri->redirect($uri->getHostGroup(), $uri->getPath(), $get);
+			}
+			
+			/**
+			 * Get SID from cookie if available, otherwise use get. Otherwise SID is NULL.
+			 * When get is used, add the sid to forcedvars to make sure it's passed on.
+			 * If we're dealing with a POST request, only allow get to make sure we're not dealing with a XSS attack
+			 */
+			if($uri->isInCookie($this->session_cookie_name)) { // if our session is in a cookie
+				$this->sid = $uri->getCookie($this->session_cookie_name);
+				$uri->forcePostVariable($this->session_cookie_name, $this->sid);
+			} elseif($uri->isInGet($this->session_cookie_name)) { // if our session is in a get parameter
+				$this->sid = $uri->getGet($this->session_cookie_name);
+				$uri->forceVariable($this->session_cookie_name, $this->sid);
+			} else { // if we have no session at all
+				$this->sid = NULL;
+			}
+		} elseif ($uri->isPost()) {
+			$this->sid = $uri->getPost($this->session_cookie_name);
+			if (is_null($this->sid)) {
+				/**
+				 * No session ID posted, we're going to check the referer.
+				 * Not posting the session ID means either XSS attack or a module which doesn't use sessions.
+				 * A refercheck must protect against XSS attacks now.
+				 */
+				$refererData = parse_url($uri->getReferer());
+				if ((!$allowInsecurePost || $uri->getHostGroup() != HostGroup::getInstance($refererData['host'], true))
+						&& !$uri->$uri->isInGet($this->session_cookie_name)
+						&& !$uri->isInCookie($this->session_cookie_name))
+					throw new SecurityException(
+							'Bad request. Please navigate to the homepage and make your request from there.',
+							'The hostname from the referer does not match the current hostname');
+			}
+		} else {
+			user_error('Unknown request method: '.$uri->getRequestMethod());
+		}
+	}
+	
+	/**
+	 * 
+	 * @param \exxprezzo\core\module\AbstractModule $module
+	 */
+	public function getSession($module) {
+		return new Session($module, $this->sid);
+		//if (!mt_rand(0,100))
+			$this->cleanup();
+	}
+	
+	/**
+	 * Remove old sessions from the database
+	 */
+	protected function cleanup() {
+		Core::$db->query('DELETE FROM `sessions` WHERE `touched`+`lifetime` < $now', array(
+			'now' => time(),
+		));
+	}
+	
+}
