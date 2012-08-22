2e4ab1e35faaa0d6a732967488aed64a0124426f
Yørn <git@yorn.priv.no>
2012-08-22 08:27:55 +0200

    Added SQL for SessionManager

---
+++ src/main/sql/session.sql
@@ -0,0 +1,46 @@
+# ************************************************************
+# Sequel Pro SQL dump
+# Version 3408
+#
+# http://www.sequelpro.com/
+# http://code.google.com/p/sequel-pro/
+#
+# Host: localhost (MySQL 5.5.25a)
+# Database: exxprezzo
+# Generation Time: 2012-08-22 06:27:27 +0000
+# ************************************************************
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+
+# Dump of table session_var
+# ------------------------------------------------------------
+
+DROP TABLE IF EXISTS `session_var`;
+
+CREATE TABLE `session_var` (
+  `session` varchar(64) NOT NULL DEFAULT '',
+  `moduleInstance` int(11) NOT NULL DEFAULT '0',
+  `key` varchar(255) NOT NULL,
+  `value` blob NOT NULL,
+  `lifetime` int(11) NOT NULL DEFAULT '3600',
+  `touched` int(11) NOT NULL,
+  PRIMARY KEY (`key`,`session`,`moduleInstance`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+
+
+
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
