b83e51a2b78dd92392bb3825a56dbda904e0a004
yo¸rn <git@yorn.priv.no>
2013-09-22 19:26:26 +0200

    Functions for reading/checking variables

--- src/main/php/exxprezzo/core/Core.php
+++ src/main/php/exxprezzo/core/Core.php
@@ -5,14 +5,18 @@ use \exxprezzo\core\output\ExceptionOutput;
 
 use \exxprezzo\core\url\AbstractUrlManager;
 
-use \ErrorException;
-use \Exception;
-
 use \exxprezzo\core\module\AbstractModule;
 use \exxprezzo\core\layout\Page;
 
 use \exxprezzo\core\exception\ExxprezzoException;
+
 use \exxprezzo\core\db\SQL;
+
+use \ArrayAccess;
+use \ErrorException;
+use \Exception;
+use \Iterator;
+use \IteratorAggregate;
 
 final class Core {
 
@@ -245,42 +249,195 @@ final class Core {
 			}
 		}
 	}
-	
-	public static function resolve($var, $path, $delim='.') {
-		assert('is_array($var) || is_object($var)');
+
+	public static function describe($var) {
+		if (is_null($var))
+			return 'NULL';
+		$result = getType($var).' ';
+		if (is_object($var))
+			$result .= ' ('.get_class($var).')';
+		elseif (is_array($var))
+			$result = var_export($var, true);
+		else
+			$result .= var_export($var, true);
+		return $result;
+	}
+
+	/**
+	 * Retrieve the value of the variable indicated in $path.
+	 * This function works by unpacking variables.
+	 * $path is a $delim separated string which gets exploded on $delim
+	 * and an iteration is performed on all segments of the path.
+	 * 
+	 * In each iteration step, we take $var and extract the name of
+	 * the part from $var and replace $var with the newly obtained value.
+	 * "extract the name" means that we will look at $var as a key/value store,
+	 * being an array, object, overloaded object, ArrayAccess etc.
+	 *
+	 * Basically $path = foo.bar could yield the same result as
+	 * $var['foo']->bar or $var->foo->getBar() or $var->getFoo()->__get('bar')
+	 */
+	public static function resolve($path, $var, $delim='.', $methodArgs=array(), &$log='') {
 		assert('is_string($path)');
+		assert('is_array($var) || is_object($var)');
 		assert('strlen($delim) == 1');
+		assert('is_array($methodArgs)');
+		if (substr($path, 0, 2) == 'd:') {
+			$path = substr($path, 2);
+			$debug = true;
+		}
+		$log .= 'Resolve '.$path.' from '.getType($var);
+		if (is_array($var))
+			$log .= ' ('.implode(', ', array_keys($var)).')';
 		if ($path == '')
 			return $var;
 		$segments = explode($delim, $path);
+		if ($path[0] == '\\') {
+			$var = call_user_func(array($segments[0], 'get'.ucfirst($segments[1])));
+			$log .= "<br>\n".$segments[0]."::".'get'.ucfirst($segments[1])."<br>\n".getType($var)."<br>\n";
+			array_shift($segments);
+			array_shift($segments);
+		}
 		$remaining = $segments;
-		while(count($segments) > 0 && count($remaining) > 0) {
+		while(!is_null($var) && count($segments) > 0 && count($remaining) > 0) {
 			$result = NULL;
 			while(is_null($result) && count($segments) > 0) {
 				$key = implode($delim, $segments);
 				$last = array_pop($segments);
 				$method = 'get'.ucfirst($key);
+				$log .= "\n<br>'".$key;
 				if (is_array($var) && isset($var[$key])) try {
+					$log .= ": arrayvar";
 					$result = $var[$key]; // Primitive array
 				} catch (Exception $e) {}
 				else if (is_object($var) && isset($var->$key)) try {
+					$log .= ": objectvar";
 					$result = $var->$key; // Direct object access
 				} catch (Exception $e) {}
 				else if (is_object($var) && $var instanceof \ArrayAccess && $var->offsetExists($key)) try {
-					$result = $var->offsetGet($key); // Object implementing ArrayAccess
+					// Object implementing ArrayAccess
+					$log .= ": ArrayAccess";
+					$result = call_user_func_array(array($var, 'offsetGet'), array_merge(array($key), $methodArgs));
 				} catch (Exception $e) {}
 				else if (is_object($var) && method_exists($var, '__get')) try {
-						$result = $var->__get($key); // Overloaded object access
+					// Overloaded object access
+					$log .= ": overload";
+					$result = call_user_func_array(array($var, '__get'), array_merge(array($key), $methodArgs));
 				} catch (Exception $e) {}
 				else if (!strpos($method, $delim) && is_object($var) && method_exists($var, $method)) try {
-					$result = $var->$method(); // Object with getter
+					$log .= ": getter";
+					$result = call_user_func_array(array($var, $method), $methodArgs); // Object with getter
 				} catch (Exception $e) {}
+				$log .= "'";
 			}
+			$log .= "<br>\n".getType($result);
+			if (is_object($result))
+				$log .= ' ('.get_class($result).')';
+			$log .= "<br>\n";
 			for($i=0;$i<count($segments)+1;$i++)
 				array_shift($remaining);
 			$segments = $remaining;
 			$var = $result;
 		}
+		if (isset($debug))
+			echo $log."<br>\n";
 		return $var;
 	}
+
+	public static function checkContains($needle, $haystack) {
+		if (is_array($haystack) || $haystack instanceof ArrayAccess)
+			return in_array($needle, $haystack);
+		elseif (is_string($haystack))
+			return strpos($haystack, $needle) !== false;
+		elseif (is_object($haystack) && $haystack instanceof IteratorAggregate)
+			return static::checkIn($needle, $haystack->getIterator());
+		elseif (is_object($haystack) && $haystack instanceof Iterator)
+			foreach($haystack as $specimen)
+				if ($specimen == $needle)
+					return true;
+		return false;
+	}
+	public static function checkFirst($needle, $haystack) {
+		if (is_array($haystack))
+			return reset($haystack) == $needle;
+		elseif (is_string($haystack))
+			return strpos($haystack, $needle) === 0;
+		elseif (is_object($haystack) && $haystack instanceof IteratorAggregate)
+			return static::checkFirst($needle, $haystack->getIterator());
+		elseif (is_object($haystack) && $haystack instanceof Iterator)
+			foreach($haystack as $specimen)
+				if ($specimen == $needle)
+					return true;
+				else
+					return false;
+		return false;
+	}
+	public static function checkLast($needle, $haystack) {
+		if (is_array($haystack))
+			return end($haystack) == $needle;
+		elseif (is_string($haystack))
+			return strpos($haystack, $needle) === strlen($haystack)-strlen($needle);
+		elseif (is_object($haystack) && $haystack instanceof IteratorAggregate)
+			return static::checkLast($needle, $haystack->getIterator());
+		elseif (is_object($haystack) && $haystack instanceof Iterator) {
+			foreach($haystack as $specimen)
+				$last = $specimen;
+			if ($specimen == $last)
+	 			return true;
+	 	}
+		return false;
+	}
+	public static function checkSubset($candidate, $subject) {
+		if ($candidate instanceof IteratorAggregate)
+			return static::checkSubset($candidate->getIterator(), $subject);
+		elseif ($subject instanceof IteratorAggregate)
+			return static::checkSubset(iterator_to_array($candidate->getIterator()), $subject);
+		elseif ($subject instanceof Iterator)
+			return static::checkSubset(iterator_to_array($candidate), $subject);
+		elseif (is_object($candidate))
+			return static::checkSubset(get_object_vars($candidate), $subject);
+		elseif (is_array($candidate) || $candidate instanceof Iterator) {
+			if (is_array($subject))
+				foreach($candidate as $key => $value)
+					if (!isset($subject[$key]) || $subject[$key] != $value)
+						return false;
+			elseif (is_object($subject))
+				foreach($candidate as $key => $value)
+					if (!isset($subject[$key]) || $subject->$key != $value)
+						return false;
+			return true;
+		}
+		return false;
+	}
+	public static function checkBegin($needle, $haystack) {
+		if (is_object($haystack) && $haystack instanceof IteratorAggregate)
+			return static::checkHas($needle, $haystack->getIterator());
+		elseif (is_array($haystack) || $haystack instanceof Iterator) {
+			end($haystack);
+			return key($haystack) == $needle;
+		}
+		return false;
+	}
+	public static function checkEnd($needle, $haystack) {
+		if (is_object($haystack) && $haystack instanceof IteratorAggregate)
+			return static::checkHas($needle, $haystack->getIterator());
+		elseif (is_array($haystack) || $haystack instanceof Iterator) {
+			reset($haystack);
+			return key($haystack) == $needle;
+		}
+		return false;
+	}
+	public static function checkHas($needle, $haystack) {
+		if (is_array($haystack) || $haystack instanceof ArrayAccess)
+			return isset($haystack[$needle]);
+		elseif (is_object($haystack) && $haystack instanceof IteratorAggregate)
+			return static::checkHas($needle, $haystack->getIterator());
+		elseif (is_object($haystack) && $haystack instanceof Iterator)
+			for($haystack as $key => $value) {
+				if ($key == $needle)
+					return true;
+			}
+		return false;
+	}
+
 }
